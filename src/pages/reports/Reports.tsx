57677a5d68c1d8342b2b5e655e003c16e2b58fd3
Selim Can Guler <selimcangler@gmail.com>
2023-12-15 13:40:23 +0300

    new reports

--- src/pages/reports/Reports.tsx
+++ src/pages/reports/Reports.tsx
@@ -9,6 +9,18 @@ const Reports = () => {
           <Anchor href="/pdf/reports/LinguistAI_Project_Information_Form.docx">
             Project Information Form
           </Anchor>
+          <Anchor
+            target="_blank"
+            href="/pdf/reports/T2314_Analysis_and_Requirements_Report.pdf"
+          >
+            Project Specification Report
+          </Anchor>
+          <Anchor
+            target="_blank"
+            href="/pdf/reports/T2314_Project_Specification_Document.pdf"
+          >
+            Analysis and Requirements Report
+          </Anchor>
         </Stack>
       </Box>
     </Container>
